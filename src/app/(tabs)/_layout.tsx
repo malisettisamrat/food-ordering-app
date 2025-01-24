4906cab79569bdae59ce91da92c5a7246c27ea21
malisettisamrat <samratmalisetti@gmail.com>
2025-01-24 15:58:57 -0800

    added flatlist to display items

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -30,7 +30,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home Page",
+          title: "Menu Page",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,39 @@
-import { Image, Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import products from "@/assets/data/products";
+import { Product } from "@/src/types";
+interface ProductListItemProps {
+  product: Product;
+}
 
-export default function TabOneScreen() {
-  const product = products[0];
+const defaultProductImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View className="flex items-center bg-white p-4 rounded-2xl justify-center">
+    <View className="flex-1 max-w-[50%] items-center bg-white p-4 rounded-2xl justify-center">
       <Image
-        source={{ uri: product.image }}
-        className="w-[100px] h-[100px] aspect-auto"
+        source={{ uri: product.image || defaultProductImage }}
+        className="w-[100%] aspect-square"
+        resizeMode="contain"
       />
       <Text className="font-bold text-xl">{product.name}</Text>
       <Text className="text-sm text-blue-700 my-2 font-semibold">
         ${product.price}
       </Text>
+    </View>
+  );
+};
+
+export default function MenuScreen() {
+  return (
+    <View className="">
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerClassName="gap-2"
+        columnWrapperClassName="gap-2"
+      />
     </View>
   );
 }
